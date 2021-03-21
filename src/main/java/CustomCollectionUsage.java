1bd9d3c35d299395c90c846c530485543b404752
lzhdanov <leonzhdan@gmail.com>
2021-03-21 22:35:37 +0300

    my iterator

--- src/main/java/CustomCollectionUsage.java
+++ src/main/java/CustomCollectionUsage.java
@@ -1,2 +1,87 @@
+import java.util.Arrays;
+
 public class CustomCollectionUsage {
+    public static void main(String[] args) {
+        MyCollection<Integer> myCollection = new MyCollection<>();
+        // add
+        myCollection.add(1);
+        myCollection.add(2);
+        myCollection.add(2);
+        myCollection.add(3);
+        //myCollection.add(null); вот это вот всё поломает
+        // size
+        System.out.println("Размер коллекции");
+        System.out.println(myCollection.size());
+        // isEmpty
+        MyCollection<Integer> emptyCollection = new MyCollection<>();
+        System.out.println("Проверка на пустоту");
+        System.out.println(emptyCollection.isEmpty());
+        System.out.println(myCollection.isEmpty());
+        // iterator
+        System.out.println("Итерация");
+        for (int item : myCollection)
+            System.out.println(item);
+        // contains
+        System.out.println("Вхождение");
+        System.out.println(myCollection.contains(2));
+        System.out.println(myCollection.contains(10));
+        // toArray of objects
+        System.out.println("массив объектов");
+        System.out.println(Arrays.toString(myCollection.toArray()));
+        // toArray of Strings
+        System.out.println("типизированный массив");
+        Integer[] intList = new Integer[2];
+        myCollection.toArray(intList);
+        System.out.println(Arrays.toString(intList));
+        Integer[] intList2 = new Integer[30];
+        myCollection.toArray(intList2);
+        System.out.println(Arrays.toString(intList2));
+        // contains all
+        System.out.println("вхождение коллекции");
+        MyCollection<Integer> findCollection = new MyCollection<>();
+        findCollection.add(1);
+        findCollection.add(2);
+        System.out.println(myCollection.containsAll(findCollection));
+        findCollection.add(10);
+        System.out.println(myCollection.containsAll(findCollection));
+        // addAll
+        System.out.println("слияние коллекций");
+        myCollection.addAll(findCollection);
+        System.out.println(Arrays.toString(myCollection.toArray()));
+        // remove
+        System.out.println("удаление");
+        myCollection.remove(2);
+        System.out.println(Arrays.toString(myCollection.toArray()));
+        myCollection.remove(10);
+        System.out.println(Arrays.toString(myCollection.toArray()));
+        // removeAll
+        System.out.println("удаление всех едениц и двоек");
+        MyCollection<Integer> removeCollection = new MyCollection<>();
+        removeCollection.add(1);
+        removeCollection.add(2);
+        myCollection.removeAll(removeCollection);
+        System.out.println(Arrays.toString(myCollection.toArray()));
+        // retainAll
+        System.out.println("убрать всех кроме 1, 2, 10");
+        myCollection.add(1);
+        myCollection.add(2);
+        myCollection.add(2);
+        myCollection.add(5);
+        myCollection.add(5);
+        myCollection.add(10);
+        System.out.println(Arrays.toString(myCollection.toArray()));
+        myCollection.retainAll(findCollection);
+        System.out.println(Arrays.toString(myCollection.toArray()));
+        // reverse
+        System.out.println("переворот");
+        myCollection.reverse();
+        System.out.println(Arrays.toString(myCollection.toArray()));
+        // clear
+        System.out.println("очистка");
+        myCollection.clear();
+        System.out.println(Arrays.toString(myCollection.toArray()));
+
+
+
+    }
 }
--- src/main/java/MyCollection.java
+++ src/main/java/MyCollection.java
@@ -1,7 +1,4 @@
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.NoSuchElementException;
+import java.util.*;
 
 public class MyCollection<E> implements Collection<E> {
 
@@ -11,6 +8,9 @@ public class MyCollection<E> implements Collection<E> {
 
     @Override
     public boolean add(E e) {
+        if(e == null){
+            throw new IllegalArgumentException("Не может быть пустое значение(?)");
+        }
         if (size == elementData.length) {
             elementData = Arrays.copyOf(elementData, (int) (size * 1.5f));
         }
@@ -25,7 +25,11 @@ public class MyCollection<E> implements Collection<E> {
 
     @Override
     public boolean isEmpty() {
-        return false;
+        boolean isEmpty = false;
+        if(this.size == 0){
+            isEmpty = true;
+        }
+        return isEmpty;
     }
 
     @Override
@@ -35,47 +39,105 @@ public class MyCollection<E> implements Collection<E> {
 
     @Override
     public boolean contains(Object o) {
-        return false;
+        boolean isContains = false;
+        for (int i = 0; i < this.size; i++){
+            if (this.elementData[i].equals(o)){
+                isContains = true;
+                break;
+            }
+        }
+        return isContains;
     }
 
     @Override
     public Object[] toArray() {
-        return new Object[0];
+        Object[] copiedArray = new Object[this.size];
+        System.arraycopy(this.elementData, 0, copiedArray, 0, this.size);
+        return copiedArray;
     }
 
     @Override
     public <T> T[] toArray(T[] a) {
-        return null;
+        System.arraycopy(this.elementData, 0, a, 0, Math.min(a.length, this.size));
+        return a;
     }
 
     @Override
     public boolean remove(Object o) {
-        return false;
+        boolean isRemoved = false;
+        if (this.contains(o)){
+            for (int i = 0; i < this.size; i++){
+                if(!isRemoved){
+                    if(this.elementData[i].equals(o)){
+                        isRemoved = true;
+                    }
+                }
+                if(isRemoved){
+                    this.elementData[i] = this.elementData[i+1];
+                }
+            }
+            this.size -= 1;
+        }
+        return isRemoved;
     }
 
     @Override
     public boolean containsAll(Collection<?> c) {
-        return false;
+        boolean isAllFound = true;
+        for (Object item : c)
+            isAllFound = this.contains(item);
+        return isAllFound;
     }
 
     @Override
     public boolean addAll(Collection<? extends E> c) {
-        return false;
+        boolean isAdd = true;
+        for (E item : c)
+            isAdd = this.add(item);
+        return isAdd;
     }
 
     @Override
     public boolean removeAll(Collection<?> c) {
-        return false;
+        boolean isRemoved = false;
+        for (Object item : c)
+            while (this.contains(item)){
+                isRemoved = this.remove(item);
+            }
+        return isRemoved;
     }
 
     @Override
     public boolean retainAll(Collection<?> c) {
-        return false;
+        boolean isRemoved = false;
+        MyCollection<Object> toRemove = new MyCollection<>();
+        for (int i = 0; i < this.size; i++) {
+            for (Object example : c){
+                if(example.equals(this.elementData[i])){
+                    toRemove.add(this.elementData[i]);
+                    break;
+                }
+            }
+        }
+        if(toRemove.size > 0){
+            isRemoved = true;
+            this.removeAll(toRemove);
+        }
+
+        return isRemoved;
     }
 
     @Override
     public void clear() {
+        this.size = 0;
+    }
 
+    public void reverse(){ //любителям на собесах заставлять переворачивать массив в блокноте посвящается
+        for (int i = 0; i < this.size / 2; i++) {
+            Object tmp = this.elementData[i];
+            this.elementData[i] = this.elementData[this.size - i - 1];
+            this.elementData[this.size - i - 1] = tmp;
+        }
     }
 
     private class MyIterator<T> implements Iterator<T> {
@@ -98,7 +160,16 @@ public class MyCollection<E> implements Collection<E> {
 
         @Override
         public void remove() {
-            throw new UnsupportedOperationException("remove");
+            if (this.cursor == 0) {
+                throw new IllegalStateException();
+            } else {
+                for (int i = cursor; i < size; i++) {
+                    elementData[cursor] = elementData[cursor++];
+                }
+                size -= 1;
+                this.cursor = -1;
+            }
         }
+
     }
 }
